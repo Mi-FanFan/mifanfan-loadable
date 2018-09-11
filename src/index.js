ff5ddaacb21d0963ef4aa12d021cd1dcb02b11aa
strong <1477869317@qq.com>
2018-09-11 18:22:14 +0800

    :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react'
 import hoistNonReactStatic from 'hoist-non-react-statics'
 
+/**
+ * {
+ *   loader: 异步获取组件的回调函数
+ *   loading: loading动画，根据状态，传入不同的参数。
+ *   timeout： 如果超过这个时间，那么会给loading组件传递一个timeOut为true的props
+ * }
+ * @param opt
+ * @returns {*}
+ * @constructor
+ */
 export default function EnhanceLoadable(opt) {
+
+  const {loader, loading: LoadingComponent, timeout} = opt
   return class EnhanceComponent extends Component {
     constructor(props) {
       super(props);
 
       this.state = {
-        component: null
+        component: null,
+        timeOut: false,
       };
     }
 
     fetchComponent = async () => {
-      const { default: component } = await importComponent();
+      const { default: component } = await loader();
 
       this.setState({
         component: component
       });
+
     }
 
     componentDidMount() {
@@ -28,7 +42,10 @@ export default function EnhanceLoadable(opt) {
     }
 
     render() {
-      return ()
+      const {component: C, timeOut} = this.state
+
+
+      return C ? <C {...this.props} /> : <LoadingComponent timeOut={timeOut}/>
     }
   }
 
